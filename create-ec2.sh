37589654b33e901e8cb762e48e8582d50206a4a1
Chandu154 <devarachandra4@gmail.com>
2023-08-24 17:37:06 +0530

    catalogue added

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,7 +8,7 @@ SECURITY_GROUP_ID="sg-076d950dcb72cc617"
 
 for i in "${NAMES[@]}"
 do
-  if [[$i == "mongodb" || $i == "mysql"]]
+  if [[ $i == "mongodb" || $i == "mysql" ]]
   then 
      INSTANCE_TYPE="t3.medium"
   else
