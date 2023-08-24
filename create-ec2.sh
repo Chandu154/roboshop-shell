a9606f49db893754ec970f3ef16dbc72d9e28931
Chandu154 <devarachandra4@gmail.com>
2023-08-24 18:53:07 +0530

    catalogue added

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,8 +3,8 @@ NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping"
 
 #if mongodb or mysql instanec type should be t3.micro ,for all others t2.micro
 INSTANCE_TYPE=""
-IMAGE_ID="ami-03265a0778a880afb"
-SECURITY_GROUP_ID="sg-076d950dcb72cc617"
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-076d950dcb72cc617
 DOMINE_NAME=devops.store
 for i in "${NAMES[@]}"
 do
@@ -16,17 +16,18 @@ do
   fi  
   echo "creating $i instance"
 
-  IP_ADDRES=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-  echo "created $i instance: $IP_ADDRES"
+  IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+  echo "created $i instance: $IP_ADDRESS"
+
    aws route53 change-resource-record-sets --hosted-zone-id Z08310291HO6SKKR1U225 --change-batch '
-    {
+   {
             "Changes": [{
             "Action": "CREATE",
                         "ResourceRecordSet": {
                             "Name": "'$i.$DOMAIN_NAME'",
                             "Type": "A",
                             "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRES'"}]
+                            "ResourceRecords": [{"Value": "'$IP_ADDRESS'"}]
                         }}]
     }
     '
