89ec4c7102f0a4b665495f9abeb2005f614435b0
Chandu154 <devarachandra4@gmail.com>
2023-08-24 17:29:25 +0530

    catalogue added

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,11 +8,12 @@ SECURITY_GROUP_ID="sg-076d950dcb72cc617"
 
 for i in "${NAMES[@]}"
 do
- if [[$i == "mongodb" || $i == "mysql"]]
+  if [[$i == "mongodb" || $i == "mysql"]]
   then 
-   INSTANCE_TYPE="t3.medium"
+     INSTANCE_TYPE="t3.medium"
   else
-   INSTANCE_TYPE="t2.micro"
- fi  
-echo "creating $i instance"
-aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications"ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+     INSTANCE_TYPE="t2.micro"
+  fi  
+  echo "creating $i instance"
+  aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications"ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+done
