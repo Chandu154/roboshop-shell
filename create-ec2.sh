8566be4b818c32e6b00fe63bd7a8fd8015895fec
Chandu154 <devarachandra4@gmail.com>
2023-08-24 17:51:37 +0530

    catalogue added

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,5 +15,8 @@ do
      INSTANCE_TYPE="t2.micro"
   fi  
   echo "creating $i instance"
-  aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+
+  IP_ADDRES=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | | jq -r '.Instances[0].PrivateIpAddress')
+
+  echo "created $i instance: $IP_ADDRES"
 done
